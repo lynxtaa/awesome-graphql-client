9b4ad3f0783eb6a090e167ce950d0d665f8f49a8
Alex Kondratyuk <lynxtaa@gmail.com>
2019-11-22 00:04:17 +0300

    chore: initial commit

---
+++ src/isResponseJSON.ts
@@ -0,0 +1,6 @@
+import { Response } from 'node-fetch'
+
+const isResponseJSON = (response: Response): boolean =>
+	(response.headers.get('Content-Type') || '').includes('application/json')
+
+export default isResponseJSON
