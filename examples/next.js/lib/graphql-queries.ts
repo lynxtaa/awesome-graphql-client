13993ff3a551c3633f945c5a7a351b2f78dd0cad
Alex Kondratyuk <lynxtaa@gmail.com>
2021-04-16 16:49:02 +0300

    chore: better config for generating hooks

--- examples/next.js/lib/graphql-queries.ts
+++ examples/next.js/lib/graphql-queries.ts
@@ -22,60 +22,56 @@ export enum CacheControlScope {
 }
 
 export type Character = {
-  __typename?: 'Character';
   /** The id of the character. */
-  id?: Maybe<Scalars['ID']>;
+  id: Maybe<Scalars['ID']>;
   /** The name of the character. */
-  name?: Maybe<Scalars['String']>;
+  name: Maybe<Scalars['String']>;
   /** The status of the character ('Alive', 'Dead' or 'unknown'). */
-  status?: Maybe<Scalars['String']>;
+  status: Maybe<Scalars['String']>;
   /** The species of the character. */
-  species?: Maybe<Scalars['String']>;
+  species: Maybe<Scalars['String']>;
   /** The type or subspecies of the character. */
-  type?: Maybe<Scalars['String']>;
+  type: Maybe<Scalars['String']>;
   /** The gender of the character ('Female', 'Male', 'Genderless' or 'unknown'). */
-  gender?: Maybe<Scalars['String']>;
+  gender: Maybe<Scalars['String']>;
   /** The character's origin location */
-  origin?: Maybe<Location>;
+  origin: Maybe<Location>;
   /** The character's last known location */
-  location?: Maybe<Location>;
+  location: Maybe<Location>;
   /**
    * Link to the character's image.
    * All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
    */
-  image?: Maybe<Scalars['String']>;
+  image: Maybe<Scalars['String']>;
   /** Episodes in which this character appeared. */
-  episode?: Maybe<Array<Maybe<Episode>>>;
+  episode: Maybe<Array<Maybe<Episode>>>;
   /** Time at which the character was created in the database. */
-  created?: Maybe<Scalars['String']>;
+  created: Maybe<Scalars['String']>;
 };
 
 export type Characters = {
-  __typename?: 'Characters';
-  info?: Maybe<Info>;
-  results?: Maybe<Array<Maybe<Character>>>;
+  info: Maybe<Info>;
+  results: Maybe<Array<Maybe<Character>>>;
 };
 
 export type Episode = {
-  __typename?: 'Episode';
   /** The id of the episode. */
-  id?: Maybe<Scalars['ID']>;
+  id: Maybe<Scalars['ID']>;
   /** The name of the episode. */
-  name?: Maybe<Scalars['String']>;
+  name: Maybe<Scalars['String']>;
   /** The air date of the episode. */
-  air_date?: Maybe<Scalars['String']>;
+  air_date: Maybe<Scalars['String']>;
   /** The code of the episode. */
-  episode?: Maybe<Scalars['String']>;
+  episode: Maybe<Scalars['String']>;
   /** List of characters who have been seen in the episode. */
-  characters?: Maybe<Array<Maybe<Character>>>;
+  characters: Maybe<Array<Maybe<Character>>>;
   /** Time at which the episode was created in the database. */
-  created?: Maybe<Scalars['String']>;
+  created: Maybe<Scalars['String']>;
 };
 
 export type Episodes = {
-  __typename?: 'Episodes';
-  info?: Maybe<Info>;
-  results?: Maybe<Array<Maybe<Episode>>>;
+  info: Maybe<Info>;
+  results: Maybe<Array<Maybe<Episode>>>;
 };
 
 export type FilterCharacter = {
@@ -98,59 +94,55 @@ export type FilterLocation = {
 };
 
 export type Info = {
-  __typename?: 'Info';
   /** The length of the response. */
-  count?: Maybe<Scalars['Int']>;
+  count: Maybe<Scalars['Int']>;
   /** The amount of pages. */
-  pages?: Maybe<Scalars['Int']>;
+  pages: Maybe<Scalars['Int']>;
   /** Number of the next page (if it exists) */
-  next?: Maybe<Scalars['Int']>;
+  next: Maybe<Scalars['Int']>;
   /** Number of the previous page (if it exists) */
-  prev?: Maybe<Scalars['Int']>;
+  prev: Maybe<Scalars['Int']>;
 };
 
 export type Location = {
-  __typename?: 'Location';
   /** The id of the location. */
-  id?: Maybe<Scalars['ID']>;
+  id: Maybe<Scalars['ID']>;
   /** The name of the location. */
-  name?: Maybe<Scalars['String']>;
+  name: Maybe<Scalars['String']>;
   /** The type of the location. */
-  type?: Maybe<Scalars['String']>;
+  type: Maybe<Scalars['String']>;
   /** The dimension in which the location is located. */
-  dimension?: Maybe<Scalars['String']>;
+  dimension: Maybe<Scalars['String']>;
   /** List of characters who have been last seen in the location. */
-  residents?: Maybe<Array<Maybe<Character>>>;
+  residents: Maybe<Array<Maybe<Character>>>;
   /** Time at which the location was created in the database. */
-  created?: Maybe<Scalars['String']>;
+  created: Maybe<Scalars['String']>;
 };
 
 export type Locations = {
-  __typename?: 'Locations';
-  info?: Maybe<Info>;
-  results?: Maybe<Array<Maybe<Location>>>;
+  info: Maybe<Info>;
+  results: Maybe<Array<Maybe<Location>>>;
 };
 
 export type Query = {
-  __typename?: 'Query';
   /** Get a specific character by ID */
-  character?: Maybe<Character>;
+  character: Maybe<Character>;
   /** Get the list of all characters */
-  characters?: Maybe<Characters>;
+  characters: Maybe<Characters>;
   /** Get a list of characters selected by ids */
-  charactersByIds?: Maybe<Array<Maybe<Character>>>;
+  charactersByIds: Maybe<Array<Maybe<Character>>>;
   /** Get a specific locations by ID */
-  location?: Maybe<Location>;
+  location: Maybe<Location>;
   /** Get the list of all locations */
-  locations?: Maybe<Locations>;
+  locations: Maybe<Locations>;
   /** Get a list of locations selected by ids */
-  locationsByIds?: Maybe<Array<Maybe<Location>>>;
+  locationsByIds: Maybe<Array<Maybe<Location>>>;
   /** Get a specific episode by ID */
-  episode?: Maybe<Episode>;
+  episode: Maybe<Episode>;
   /** Get the list of all episodes */
-  episodes?: Maybe<Episodes>;
+  episodes: Maybe<Episodes>;
   /** Get a list of episodes selected by ids */
-  episodesByIds?: Maybe<Array<Maybe<Episode>>>;
+  episodesByIds: Maybe<Array<Maybe<Episode>>>;
 };
 
 
@@ -207,16 +199,7 @@ export type GetCharactersQueryVariables = Exact<{
 }>;
 
 
-export type GetCharactersQuery = (
-  { __typename?: 'Query' }
-  & { characters?: Maybe<(
-    { __typename?: 'Characters' }
-    & { results?: Maybe<Array<Maybe<(
-      { __typename?: 'Character' }
-      & Pick<Character, 'id' | 'name'>
-    )>>> }
-  )> }
-);
+export type GetCharactersQuery = { characters: Maybe<{ results: Maybe<Array<Maybe<Pick<Character, 'id' | 'name'>>>> }> };
 
 
 export const GetCharactersDocument = `
