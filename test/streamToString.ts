318b2c387b70f4d3ba1fb7ecad9a9ea1054ce041
Alex Kondratyuk <lynxtaa@gmail.com>
2022-02-11 20:27:46 +0300

    chore: refactor tests

--- test/streamToString.ts
+++ test/streamToString.ts
@@ -1,11 +1,9 @@
 export async function streamToString(stream: NodeJS.ReadableStream): Promise<string> {
 	const chunks: Buffer[] = []
 
-	await new Promise<void>((resolve, reject) => {
-		stream.on('data', chunk => chunks.push(Buffer.from(chunk)))
-		stream.on('error', reject)
-		stream.on('end', resolve)
-	})
+	for await (const chunk of stream) {
+		chunks.push(Buffer.from(chunk))
+	}
 
 	return Buffer.concat(chunks).toString('utf8')
 }
