427d0074d2a2356d802c603c0ec869f4426b87dc
Water.Li <lizongbin@hotmail.com>
2022-10-17 20:49:29 +0800

    Update GraphQLRequestError.ts

--- src/GraphQLRequestError.ts
+++ src/GraphQLRequestError.ts
@@ -6,25 +6,29 @@ export class GraphQLRequestError<
 	query: string
 	variables?: Record<string, unknown>
 	response!: TResponse
+	extensions?: Record<string, unknown>
 
 	constructor({
 		query,
 		variables,
 		response,
 		message,
+		extensions,
 	}: {
 		query: string
 		variables?: Record<string, unknown>
 		response: TResponse
 		message: string
+		extensions?: Record<string, unknown>
 	}) {
 		super(`GraphQL Request Error: ${message}`)
-
 		this.query = query
 		if (variables) {
 			this.variables = variables
 		}
-
+		if (extensions) {
+			this.extensions = extensions
+		}
 		// Hide Response from `console.log(error)`
 		Object.defineProperty(this, 'response', {
 			enumerable: false,
