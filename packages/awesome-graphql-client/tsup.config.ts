6e54c8af0df67a4e8dfbdac03a045df8b1f672bd
Alex Kondratiuk <lynxtaa@gmail.com>
2023-09-15 20:14:24 +0200

    chore: use tsup (#84)

---
+++ packages/awesome-graphql-client/tsup.config.ts
@@ -0,0 +1,37 @@
+import { copyFile } from 'node:fs/promises'
+
+import { defineConfig, Options } from 'tsup'
+
+export default defineConfig(options => {
+	const commonOptions: Partial<Options> = {
+		entry: {
+			'awesome-graphql-client': 'src/index.ts',
+		},
+		...options,
+	}
+
+	return [
+		{
+			...commonOptions,
+			format: ['esm'],
+			minify: true,
+			outExtension: () => ({ js: '.mjs' }),
+			dts: true,
+			clean: true,
+			async onSuccess() {
+				// Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
+				await copyFile(
+					'dist/awesome-graphql-client.mjs',
+					'dist/awesome-graphql-client.legacy-esm.js',
+				)
+			},
+		},
+		{
+			...commonOptions,
+			format: 'cjs',
+			minify: false,
+			outDir: './dist/cjs/',
+			outExtension: () => ({ js: '.cjs' }),
+		},
+	]
+})
