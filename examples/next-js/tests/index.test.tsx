21f440a0c5ad9b194afe2cad4dcc4bc5df45bac9
Alex Kondratiuk <lynxtaa@gmail.com>
2023-10-03 22:49:14 +0200

    chore: update deps

--- examples/next-js/tests/index.test.tsx
+++ examples/next-js/tests/index.test.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { userEvent } from '@testing-library/user-event'
 
 import { server, graphql } from '../jest/testServer'
 import { GetCharactersQuery, GetCharactersQueryVariables } from '../lib/graphql-queries'
