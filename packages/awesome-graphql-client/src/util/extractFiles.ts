5dcf167eb9f5d293401d1b616ff9af4fa2f1174f
Alex Kondratiuk <lynxtaa@gmail.com>
2024-10-14 23:28:12 +0200

    types

--- packages/awesome-graphql-client/src/util/extractFiles.ts
+++ packages/awesome-graphql-client/src/util/extractFiles.ts
@@ -1,14 +1,36 @@
-const isArray = (value: unknown): value is unknown[] | FileList =>
+interface FileList {
+	/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/FileList/length) */
+	readonly length: number
+	/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/FileList/item) */
+	// eslint-disable-next-line @typescript-eslint/method-signature-style
+	item(index: number): File | null
+	[index: number]: File
+
+	prototype: FileList
+	new (): this
+}
+
+declare global {
+	const FileList: FileList | undefined
+}
+
+const isIterable = (value: unknown): value is unknown[] | FileList =>
 	Array.isArray(value) || (typeof FileList !== 'undefined' && value instanceof FileList)
 
 // https://github.com/sindresorhus/is-plain-obj/blob/main/index.js
 function isPlainObject(value: unknown): value is Record<string, unknown> {
-	if (Object.prototype.toString.call(value) !== '[object Object]') {
+	if (typeof value !== 'object' || value === null) {
 		return false
 	}
 
 	const prototype = Object.getPrototypeOf(value)
-	return prototype === null || prototype === Object.prototype
+	return (
+		(prototype === null ||
+			prototype === Object.prototype ||
+			Object.getPrototypeOf(prototype) === null) &&
+		!(Symbol.toStringTag in value) &&
+		!(Symbol.iterator in value)
+	)
 }
 
 /**
@@ -40,7 +62,7 @@ export function extractFiles<TUpload>(
 			}
 			saved.push(paths.join('.'))
 			target[currentPath] = null
-		} else if (isArray(value) || isPlainObject(value)) {
+		} else if (isIterable(value) || isPlainObject(value)) {
 			if (stackSet.has(value)) {
 				throw new Error(`Circular dependency detected in ${paths.join('.')}`)
 			}
@@ -48,7 +70,7 @@ export function extractFiles<TUpload>(
 
 			let newObj: unknown[] | Record<string, unknown>
 
-			if (isArray(value)) {
+			if (isIterable(value)) {
 				newObj = []
 				// eslint-disable-next-line unicorn/no-for-loop
 				for (let i = 0; i < value.length; i++) {
