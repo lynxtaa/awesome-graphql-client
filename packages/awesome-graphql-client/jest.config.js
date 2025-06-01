f227a209c413a92858d8dce4eca2447a3bc0d1d2
Alex Kondratiuk <lynxtaa@gmail.com>
2025-06-01 22:48:07 +0300

    chore: update dependencies, drop CommonJS build (BREAKING CHANGE) (#102)

--- packages/awesome-graphql-client/jest.config.js
+++ packages/awesome-graphql-client/jest.config.js
@@ -1,13 +1,14 @@
 const isCI = process.env.CI !== undefined
 
 /** @type {import('jest').Config} */
-module.exports = {
+export default {
 	resetMocks: true,
 	roots: ['<rootDir>/test', '<rootDir>/src'],
 	coverageReporters: isCI ? ['clover', 'json', 'lcov', 'text'] : ['html', 'text-summary'],
 	coveragePathIgnorePatterns: ['<rootDir>/test'],
 	setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
 	transform: {
-		'^.+\\.(t|j)sx?$': ['@swc/jest'],
+		'^.+\\.(t|j)sx?$': ['@swc-node/jest'],
 	},
+	extensionsToTreatAsEsm: ['.ts'],
 }
