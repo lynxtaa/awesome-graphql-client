46bdd4e16e1914c9f1898d986c1104f263d24a0e
Alex Kondratiuk <lynxtaa@gmail.com>
2025-06-01 21:49:57 +0200

    chore: drop Node 18 from README

--- packages/awesome-graphql-client/test/node.test.ts
+++ packages/awesome-graphql-client/test/node.test.ts
@@ -2,10 +2,7 @@
  * @jest-environment node
  */
 
-import { createReadStream, statSync, readFileSync } from 'node:fs'
-import path from 'node:path'
-import { Readable } from 'node:stream'
-import { ReadableStream as WebReadableStream } from 'node:stream/web'
+import { readFileSync } from 'node:fs'
 
 import { type FileUpload, GraphQLUpload } from 'graphql-upload'
 
@@ -14,10 +11,6 @@ import { gql } from '../src/util/gql'
 
 import { createServer, type TestServer } from './jest/gqlServer'
 import { streamToString } from './streamToString'
-
-const maybeDescribe = (condition: boolean) => (condition ? describe : describe.skip)
-
-const nodeMajorVersion = Number(process.versions.node.split('.')[0])
 
 let server: TestServer
 
@@ -56,17 +49,18 @@ describe('fetch', () => {
 	})
 })
 
-maybeDescribe(nodeMajorVersion < 20)('node < 20', () => {
-	it('streams a file', async () => {
-		interface UploadFile {
-			uploadFile: boolean
-		}
-		interface UploadFileVariables {
-			file: any
-		}
+it('streams a file using openAsBlob', async () => {
+	const { openAsBlob } = await import('node:fs')
 
-		server = await createServer(
-			`
+	interface UploadFile {
+		uploadFile: boolean
+	}
+	interface UploadFileVariables {
+		file: any
+	}
+
+	server = await createServer(
+		`
 				scalar Upload
 				type Mutation {
 					uploadFile(file: Upload!): Boolean
@@ -75,126 +69,38 @@ maybeDescribe(nodeMajorVersion < 20)('node < 20', () => {
 					hello: String!
 				}
 			`,
-			{
-				Upload: GraphQLUpload as any,
-				Mutation: {
-					async uploadFile(_, { file }: { file: Promise<FileUpload> }) {
-						// eslint-disable-next-line @typescript-eslint/unbound-method
-						const { filename, createReadStream } = await file
-						expect(filename).toBe('data.txt')
-						const str = await streamToString(createReadStream())
-						expect(str).toBe(readFileSync('./test/fixtures/data.txt', 'utf8'))
+		{
+			Upload: GraphQLUpload as any,
+			Mutation: {
+				async uploadFile(_, { file }: { file: Promise<FileUpload> }) {
+					// eslint-disable-next-line @typescript-eslint/unbound-method
+					const { filename, createReadStream } = await file
+					expect(filename).toBe('data.txt')
+					const str = await streamToString(createReadStream())
+					expect(str).toBe(readFileSync('./test/fixtures/data.txt', 'utf8'))
 
-						return true
-					},
+					return true
 				},
 			},
-		)
+		},
+	)
 
-		// https://github.com/nodejs/undici/issues/2202#issuecomment-1664134203
-		class StreamableFile extends Blob {
-			#filePath: string
-			name: string
-			lastModified: number
-			override type: string
-
-			constructor(filePath: string) {
-				const { mtime, size } = statSync(filePath)
-
-				super([])
-
-				this.name = path.parse(filePath).base
-				this.lastModified = mtime.getTime()
-				this.#filePath = filePath
-				this.type = ''
-
-				Object.defineProperty(this, 'size', {
-					value: size,
-					writable: false,
-				})
-				Object.defineProperty(this, Symbol.toStringTag, {
-					value: 'File',
-					writable: false,
-				})
-			}
-
-			override stream(): WebReadableStream<any> {
-				return Readable.toWeb(createReadStream(this.#filePath))
-			}
-		}
-
-		const client = new AwesomeGraphQLClient({
-			endpoint: server.endpoint,
-		})
-
-		const query = gql`
-			mutation UploadFile($file: Upload!) {
-				uploadFile(file: $file)
-			}
-		`
-
-		const data = await client.request<UploadFile, UploadFileVariables>(query, {
-			file: new StreamableFile('./test/fixtures/data.txt'),
-		})
-
-		expect(data).toEqual({ uploadFile: true })
+	const client = new AwesomeGraphQLClient({
+		endpoint: server.endpoint,
 	})
-})
 
-maybeDescribe(nodeMajorVersion >= 20)('node >= 20', () => {
-	it('streams a file using openAsBlob', async () => {
-		const { openAsBlob } = await import('node:fs')
-
-		interface UploadFile {
-			uploadFile: boolean
+	const query = gql`
+		mutation UploadFile($file: Upload!) {
+			uploadFile(file: $file)
 		}
-		interface UploadFileVariables {
-			file: any
-		}
+	`
 
-		server = await createServer(
-			`
-				scalar Upload
-				type Mutation {
-					uploadFile(file: Upload!): Boolean
-				}
-				type Query {
-					hello: String!
-				}
-			`,
-			{
-				Upload: GraphQLUpload as any,
-				Mutation: {
-					async uploadFile(_, { file }: { file: Promise<FileUpload> }) {
-						// eslint-disable-next-line @typescript-eslint/unbound-method
-						const { filename, createReadStream } = await file
-						expect(filename).toBe('data.txt')
-						const str = await streamToString(createReadStream())
-						expect(str).toBe(readFileSync('./test/fixtures/data.txt', 'utf8'))
+	const blob = await openAsBlob('./test/fixtures/data.txt')
+	const file = new File([blob], 'data.txt')
 
-						return true
-					},
-				},
-			},
-		)
-
-		const client = new AwesomeGraphQLClient({
-			endpoint: server.endpoint,
-		})
-
-		const query = gql`
-			mutation UploadFile($file: Upload!) {
-				uploadFile(file: $file)
-			}
-		`
-
-		const blob = await openAsBlob('./test/fixtures/data.txt')
-		const file = new File([blob], 'data.txt')
-
-		const data = await client.request<UploadFile, UploadFileVariables>(query, {
-			file,
-		})
-
-		expect(data).toEqual({ uploadFile: true })
+	const data = await client.request<UploadFile, UploadFileVariables>(query, {
+		file,
 	})
+
+	expect(data).toEqual({ uploadFile: true })
 })
